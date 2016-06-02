d07170a3903db1c2feceacb6700cb241447b8597
tianzx <436011080@qq.com>
2016-06-02 21:50:59 +0800

    V0.2

--- src/main/java/net/tianzx/TankClient.java
+++ src/main/java/net/tianzx/TankClient.java
@@ -1,6 +1,8 @@
 package net.tianzx;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -29,6 +31,7 @@ public class TankClient extends Frame {
         });
         setResizable(false);
         setBackground(Color.GREEN);
+        this.addKeyListener(new KeyMonitor());
         setVisible(true);
 
         new Thread(new PaintThread()).start();
@@ -39,7 +42,8 @@ public class TankClient extends Frame {
         g.setColor(Color.red);
         g.fillOval(x, y, 30, 30);
         g.setColor(c);
-        y += 5;
+//        y += 5;
+        System.err.println("painting..............");
     }
 
     @Override
@@ -72,6 +76,28 @@ public class TankClient extends Frame {
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+            }
+        }
+    }
+
+    private class KeyMonitor extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            super.keyPressed(e);
+//            System.err.println("begin listening....");
+            int key = e.getKeyCode();
+//            if(key ==KeyEvent.VK_RIGHT){
+//                x+=5;
+//            }
+            switch (key){
+                case KeyEvent.VK_LEFT:x-=5;
+                    break;
+                case KeyEvent.VK_UP:y-=5;
+                    break;
+                case KeyEvent.VK_RIGHT: x+=5;
+                    break;
+                case KeyEvent.VK_DOWN:y+=5;
+                    break;
             }
         }
     }
