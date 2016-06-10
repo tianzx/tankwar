c88c223402a00becb963d873acfee76d493bdcca
tianzx <436011080@qq.com>
2016-06-10 13:10:23 +0800

    V2.6

--- src/main/java/net/tianzx/NetClient.java
+++ src/main/java/net/tianzx/NetClient.java
@@ -2,6 +2,7 @@ package net.tianzx;
 
 import net.tianzx.test2.Test2;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 
@@ -10,12 +11,29 @@ import java.net.Socket;
  */
 public class NetClient {
 
+    private static int UDP_PORT_START = 2223;
+    private int udpPort;
+
+    public NetClient(){
+        udpPort = UDP_PORT_START++;
+    }
     public void connect(String ip,int port){
+        Socket s =null;
         try {
-            Socket s = new Socket(ip,port);
+             s = new Socket(ip,port);
+            DataOutputStream dos = new DataOutputStream(s.getOutputStream());
+            dos.writeInt(udpPort);
             System.err.print("Connect to Server");
         } catch (IOException e) {
             e.printStackTrace();
+        }finally {
+            if (null!=s){
+                try {
+                    s.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
--- src/main/java/net/tianzx/TankServer.java
+++ src/main/java/net/tianzx/TankServer.java
@@ -1,8 +1,11 @@
 package net.tianzx;
 
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by tianzx on 2016/6/10.
@@ -11,15 +14,36 @@ public class TankServer {
 
     public   static final int TCP_PORT = 8888;
 
-    public static void main(String[] args) {
+    List<Client> clients = new ArrayList<Client>();
+
+    public void start(){
         try {
             ServerSocket ss = new ServerSocket(TCP_PORT);
             while (true) {
                 Socket s = ss.accept();
-                System.err.println(s.getInetAddress() +" " +s.getPort());
+                DataInputStream dis = new DataInputStream(s.getInputStream());
+                String ip = s.getInetAddress().getHostAddress();
+                int udpPot = dis.readInt();
+                Client c = new Client(ip,udpPot);
+                clients.add(c);
+                s.close();
             }
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new TankServer().start();
+    }
+
+    private class Client {
+        String ip;
+        int udpPort;
+
+        public  Client(String ip,int udpPort){
+            this.ip = ip;
+            this.udpPort=udpPort;
         }
     }
 }
